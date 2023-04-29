1d120521acf11882a27737a969785b8a1074d44c
Shenna Mae Tan <shenmae.t@gmail.com>
2023-04-29 16:40:30 +0800

    feat: landing page set up with assets folder update

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ function App() {
 	return (
 		<div>
 			<h1>WorkWise</h1>
+			<Landing />
 		</div>
     );
 }
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,10 +1,31 @@
 import logo from '../assets/logo.svg'
+import main from '../assets/main.svg'
 
 const Landing = () => {
     return (
-        <div>
-            <h1>WorkWise</h1>
-        </div>
+        <main>
+            <nav>
+                <img src={logo} alt ='WorkWise' classname='logo'/>
+            </nav>
+            <div className='container page'>
+                {/* information */}
+                <div className='info'>
+                    <h1>
+                        job <span>tracking</span> app
+                    </h1>
+                    <p>
+                    WorkWise is a user-friendly web app designed to help job seekers easily track their job applications.
+                    With WorkWise, users can their applications, set reminders for follow-ups, and identify which areas their can improve in for interviews.
+                    <br/>
+                    By keeping all their job search information in one place, WorkWise empowers job seekers to stay organised and focused on landing their dream job.
+                    </p>
+                    <button className='btn btn-hero'>
+                        Login/Register
+                    </button>
+                </div>
+                <img src={main} alt='application logging' className='img main-img'/>
+            </div>
+        </main>
     )
 }
 
