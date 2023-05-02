2654c100072a9f9bfb38cca7da66a61e66e3bfff
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-02 23:32:37 +0800

    chore: temp nav links for easy nav

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,19 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 function App() {
 	return (
         <BrowserRouter>
+        <nav>
+            <Link to='/'>Dashboard</Link>
+            <Link to='/register'>Register</Link>
+            <Link to='/landing'>Landing</Link>
+        </nav>
             <Routes>
                 <Route path='/' element={<div>Dashboard</div>}/>
-                <Route path='register' element={<div>Register</div>}/>
+                <Route path='/register' element={<div>Register</div>}/>
                 <Route path='/landing' element={<Landing />}/>
                 <Route path='*' element={<h1>Error</h1>}/>
             </Routes>
         </BrowserRouter>
-    );
+    )
 }
 
 export default App
