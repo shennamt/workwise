8921c7b7ed963e4e15f891b9f502e105738f27ad
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-02 16:05:53 +0800

    chore: added notes for later review

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
     return (
         <main>
             <nav>
-                <img src={logo} alt ='WorkWise' classname='logo'/>
+                <img src={logo} alt ='workwise' classname='logo'/>
             </nav>
             <div className='container page'>
                 {/* information */}
@@ -13,17 +13,21 @@ const Landing = () => {
                     <h1>
                         job <span>tracking</span> app
                     </h1>
+                    
                     <p>
                     WorkWise is a user-friendly web app designed to help job seekers easily track their job applications.
                     With WorkWise, users can their applications, set reminders for follow-ups, and identify which areas their can improve in for interviews.<br/>
                     <br/>
                     By keeping all your job search information in one place, WorkWise empowers job seekers to stay organised and focused on landing your dream job.
                     </p>
+
                     <button className='btn btn-hero'>
                         Login/Register
                     </button>
                 </div>
+
                 <img src={main} alt='application logging' className='img main-img'/>
+                {/* PS: img is main class and the other class is for styling on smaller media */}
             </div>
         </main>
     )
