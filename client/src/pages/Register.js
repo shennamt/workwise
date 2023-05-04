a911b5e110fecf8550768add3c3eaf76a545771d
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-04 13:48:25 +0800

    feat: basic set up for useState and logging

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { Logo } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
 
+// Default state
 const initialState = {
     name: '',
     email: '',
@@ -10,6 +11,21 @@ const initialState = {
 }
 
 const Register = () => {
+
+    const [values, setValues] =  useState(initialState)
+
+    // global state and useNavigate
+
+    // logs to show which input we're working on
+    const handleChange = (e) => {
+        console.log(e.target)
+    }
+
+    // looking for event Obj and log the form
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target)
+    }
     return (
         <h1>Register</h1>
     )
