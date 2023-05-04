2fc94f4943712a151e5ea4f6ef243a23429b99cd
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-04 22:46:29 +0800

    feat: connected db

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './db/connect.js'
 
 const app = express()
+dotenv.config()
 
 // middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
 app.get('/', (req, res) => {
-    throw new Error("testing error")
     res.send("The healthy server life chose YOU!")
 })
 
@@ -16,6 +18,16 @@ app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5001
 
-app.listen(port, () => {
-    console.log(`Server is listening on ${port}...`)
-})
+// connectDB returns a promise so you need async await
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Server is listening on ${port}...`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
