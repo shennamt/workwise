c69e73166d8dd569e37be0dd723083cb29699ff7
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-04 17:19:29 +0800

    chore: todo for actions and first dispatch

---
+++ client/src/context/actions.js
@@ -0,0 +1,8 @@
+// dispatch display alert if trying to set up a form and the value is empty
+// set up var = show alert
+// then set up import in reducer and appcontext
+// in appcontext, set up display alert function and dispatch
+// in reducer, handle function to return curr state, then change the value of showalert
+// create alert component also
+// rmb that alert type and text is alrdy in state, in global context
+// so use import useAppContext to look for the alert stuff 
