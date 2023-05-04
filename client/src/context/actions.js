748826ab7c1b3a4318653db6aeb2d284362473fc
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-04 19:21:21 +0800

    chore: comments for todo on clearing alert

--- client/src/context/actions.js
+++ client/src/context/actions.js
@@ -1,10 +1,7 @@
-// dispatch display alert if trying to set up a form and the value is empty
-// set up var = show alert, use uppercase to avoid mistakes haha
-// then set up import in reducer and appcontext
-// in appcontext, set up display alert function and dispatch
-// in reducer, handle function to return curr state, then change the value of showalert
-// create alert component also
-// rmb that alert type and text is alrdy in state, in global context
-// so use import useAppContext to look for the alert stuff
-
 export const DISPLAY_ALERT = 'SHOW_ALERT'
+
+// add a clear alert function
+// export the clear alert action here
+// import in reducer and app context
+// in reducer, create the action type with the alert set back to false, text and type empty
+// in appcontext, dispatch the display alert, the create a const for clearalert function. maybe... three secs?
