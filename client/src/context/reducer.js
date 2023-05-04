4107696f8577ba884ee2b9f3026117de16db89a1
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-04 17:09:21 +0800

    feat: reducer set up

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -2,3 +2,10 @@
 // import into appContext.js
 // then replace dummy state with useReducer
 // pass function from reducer to access state
+
+const reducer = (state, action) => {
+    // run if we dispatch action with no handler
+    throw new Error(`no such action : ${action.type}`)
+}
+
+export default reducer
