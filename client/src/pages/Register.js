c8a2e7e44cd235c55fbc39c80ab78d1ed882c0f3
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-04 13:42:44 +0800

    feat: setting up initialState

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,3 +1,14 @@
+import { useState, useEffect } from 'react'
+import { Logo } from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+}
+
 const Register = () => {
     return (
         <h1>Register</h1>
