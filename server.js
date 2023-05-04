089db8c241e1e21cd45b345e385cb96be3d7a8d8
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-04 21:25:02 +0800

    feat: server set up with port 5001

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Server running...')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('The healthy server life chose YOU!')
+})
+
+const port = process.env.PORT || 5001
+
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}...`)
+})
