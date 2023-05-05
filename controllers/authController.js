98fbea1965ee3c2425f71beb4442bca809027006
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-06 00:28:57 +0800

    feat: password successfully hashed tested on postman

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,7 @@ const register = async (req, res) => {
     }
 
     const user = await User.create({ name, email, password })
-    res.status(StatusCodes.CREATED).json({ user })
+    res.status(StatusCodes.OK).json({ user })
 }
 
 const login = async (req, res) => {
