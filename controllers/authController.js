091aeb826c925dcbd4909373cf054399e67a0ff7
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-05 18:30:57 +0800

    feat: created custom API err

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,25 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
 
+// custom error class that extends built in error class
+// constructor takes msg param and pass to parent err class constructor using super()
+// thereby setting msg property of err to passed val
+// custom class also adds statuscode.
+// this class throws errs with specific http stat codes.
+
+class CustomAPIError extends Error {
+    constructor(message) {
+        super(message)
+        this.statusCode = StatusCodes.BAD_REQUEST
+    }
+}
+
+
 const register = async (req, res) => {
     const { name, email, password } = req.body
     
     if (!name || !email || !password) {
-        throw new Error("Did you miss an input?")
+        throw new CustomAPIError("Did you miss an input?")
     }
 
     const user = await User.create({ name, email, password })
