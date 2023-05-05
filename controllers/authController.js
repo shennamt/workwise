be2985e69d9badadc173b5064e01a4f9ea39dbff
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-05 19:00:59 +0800

    feat: customeAPIErr refactor

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,6 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
 
-// custom error class that extends built in error class
-// constructor takes msg param and pass to parent err class constructor using super()
-// thereby setting msg property of err to passed val
-// custom class also adds statuscode.
-// this class throws errs with specific http stat codes.
-
-class CustomAPIError extends Error {
-    constructor(message) {
-        super(message)
-    }
-}
-
 class BadRequestError extends CustomAPIError {
     constructor(message) {
         super(message)
