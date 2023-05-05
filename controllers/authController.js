cb3c88e99bb02ac4f7337674dae4c29d35ed106f
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-05 15:12:43 +0800

    chore: npm i express-async-errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,6 @@
 import User from '../models/User.js'
 
 const register = async (req, res, next) => {
-
     try {
         const user = await User.create(req.body)
         res.status(201).json({ user })
