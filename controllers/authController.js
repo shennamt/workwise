a4293c4a52250c1391e16b30c527ce973e41fdde
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-05 18:43:03 +0800

    feat: badreq and notfound error class

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,16 +10,28 @@ import { StatusCodes } from 'http-status-codes'
 class CustomAPIError extends Error {
     constructor(message) {
         super(message)
+    }
+}
+
+class BadRequestError extends CustomAPIError {
+    constructor(message) {
+        super(message)
         this.statusCode = StatusCodes.BAD_REQUEST
     }
 }
 
+class NotFoundError extends CustomAPIError {
+    constructor(message) {
+        super(message)
+        this.statusCode = StatusCodes.NOT_FOUND
+    }
+}
 
 const register = async (req, res) => {
     const { name, email, password } = req.body
     
     if (!name || !email || !password) {
-        throw new CustomAPIError("Did you miss an input?")
+        throw new BadRequestError("Did you miss an input?")
     }
 
     const user = await User.create({ name, email, password })
