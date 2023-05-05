be1b28dfaa6fbb350bf47ea28d41eb96b93ec518
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-05 19:12:27 +0800

    feat: error files populated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,6 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
 
-class BadRequestError extends CustomAPIError {
-    constructor(message) {
-        super(message)
-        this.statusCode = StatusCodes.BAD_REQUEST
-    }
-}
-
-class NotFoundError extends CustomAPIError {
-    constructor(message) {
-        super(message)
-        this.statusCode = StatusCodes.NOT_FOUND
-    }
-}
-
 const register = async (req, res) => {
     const { name, email, password } = req.body
     
