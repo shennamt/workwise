2efbc474dc0d8b4f63a70aa7dfa09e2f7da35dd3
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-05 16:00:18 +0800

    feat: err handle for missing fields

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,9 +4,21 @@ const errorHandlerMiddleware = (err, req, res, next)  => {
     console.log(err)
     const defaultError = {
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        msg:"Oops, something went wrong... try again later"
+        msg: "Oops, something went wrong... try again later",
     }
-    res.status(defaultError.statusCode).json({ msg: err })
+    if (err.name === 'ValidationError') {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        // defaultError.msg = err.message
+        defaultError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(',')
+    }
+    // res.status(defaultError.statusCode).json({ msg: err })
+    res.status(defaultError.statusCode).json({ msg: defaultError.msg })
 }
+
+// right now we're sending a huge err object with the err properties which is useless
+// let send 400 since it's a bad req
+// then iterate over the err obj and grab the message only
 
 export default errorHandlerMiddleware
