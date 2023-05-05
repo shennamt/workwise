cfb176ce5f882121896cc187f34dde0775755f8e
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-05 15:08:22 +0800

    chore: updating error handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,12 @@
 import User from '../models/User.js'
 
-const register = async (req, res) => {
+const register = async (req, res, next) => {
 
     try {
         const user = await User.create(req.body)
         res.status(201).json({ user })
     } catch (error) {
-        console.log(error)
-        res.status(500).json({ msg: "Uh oh... There was an error" })
+        next(error)
     }
 }
 
