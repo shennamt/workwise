b50bdd80280e36400552d6dbb3cb4550f5628e51
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-06 10:22:03 +0800

    chore: proxy set up + explanation in branch comment

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,21 @@
+import { useEffect } from 'react'
+
 const Dashboard = () => {
+    const fetchData = async () => {
+        try {
+            // if not fetching from correct server, wil get cors prob cos not permitted loading resources
+            // like if i made a stupid data.json file and replaced that in the fetch, it would work.
+            // const response = await fetch('/data.json')
+            const response = await fetch('/api/v1')
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
     return (
         <h1>Dashboard</h1>
     )
