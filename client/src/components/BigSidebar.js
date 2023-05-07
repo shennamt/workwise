98a38dab0b7b515c80787d542bcf64c0aa6f09a6
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-07 16:42:07 +0800

    chore: psuedocode for setup

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -9,5 +9,9 @@ const BigSidebar = () => {
         </Wrapper>
     )
 }
+// import use app, wrapper, navlinks
+// use showsidebar from context
+// show side bar based on css toggle boolian
+// consten to be logo and navlinks
 
 export default BigSidebar
