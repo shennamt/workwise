751bc53addfd4d2d0d99573eff01aed3ebd0b255
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-07 17:59:37 +0800

    chore: basic auth setup works. tested using postman

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,10 @@
+import { UnAuthenticatedError } from '../errors/index.js'
+
 const auth = async (req, res, next) => {
-    const headers = req.headers
     const authHeader = req.headers.authorization
-    console.log(headers)
-    console.log(authHeader)
+    if (!authHeader) {
+        throw new UnAuthenticatedError('Oops... Authentication Invalid')
+    }
     next()
 }
 
