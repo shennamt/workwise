aef58e758de8a1560155c09e61c89f94906ae542
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-07 23:27:38 +0800

    chore: setting up the profile page component

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,6 +1,31 @@
+import { useState } from 'react'
+import { FormRow, Alert } from '../../components'
+import { useAppContext } from '../../context/appContext'
+
 const Profile = () => {
+    const { user, showAlert, displayAlert, updateUser, isLoading } = useAppContext()
+    const [name, setName] = useState(user?.name)
+    const [email, setEmail] = useState(user?.email)
+    const [location, setLocation] = useState(user?.location)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("update user")
+    }
+
     return (
-        <h1>Profile Page</h1>
+        <form className='form' onSubmit={handleSubmit}>
+            <h3>Profile here</h3>
+            {showAlert && <Alert />}
+            <div className='form-center'>
+                <FormRow
+                    type='text'
+                    name='name'
+                    value={name}
+                    handleChange={(e) => setName(e.target.value)}
+                />
+            </div>
+        </form>
     )
 }
 
