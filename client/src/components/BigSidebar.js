20ace4d79925de8480c23839e315bcfb99db58dd
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-07 16:56:16 +0800

    feat: big side bar set up donez

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,11 +1,20 @@
 import Wrapper from '../assets/wrappers/BigSidebar'
+import NavLinks from './NavLinks'
+import { useAppContext } from '../context/appContext'
 
 const BigSidebar = () => {
+    // pass toggle sidebar in navlinks if it's annoying. let's see how
+    const { showSidebar } = useAppContext()
+
     return (
         <Wrapper>
-            <h4>
-                BigSidebar
-            </h4>
+            {/* since show sidebar will be false by default, display it on false */}
+            <div className={showSidebar? 'sidebar-container' : 'sidebar-container show-sidebar'}>
+                <div className='content'>
+                    <h3><span>WorkWise</span></h3>
+                    <NavLinks/>
+                </div>
+            </div>
         </Wrapper>
     )
 }
