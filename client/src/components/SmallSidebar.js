d5d4b626d450689dcffe929119b3b949bd9c7ffd
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-07 14:57:23 +0800

    feat: modal window set up

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,11 +1,20 @@
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import { FaTimes } from 'react-icons/fa'
+import Logo from './Logo'
+import links from '../utils/links'
+import { useAppContext } from '../context/appContext'
+import { NavLink } from 'react-router-dom'
 
 const SmallSidebar = () => {
     return (
         <Wrapper>
-            <h4>
-                SmallSidebar
-            </h4>
+            <div className='sidebar-container show-sidebar'>
+                <div className='content'>
+                    <button type='button' className='close-btn' onClick={() => {console.log('toggling sidebar')}}>
+                        <FaTimes />
+                    </button>
+                </div>
+            </div>
         </Wrapper>
     )
 }
