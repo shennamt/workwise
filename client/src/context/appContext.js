6473109ceb6e776e56fa08279ed9b402a7246282
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-08 00:53:42 +0800

    feat: basic axios set up works

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -85,7 +85,16 @@ const AppProvider = ({ children }) => {
     }
 
     const updateUser = async (currentUser) => {
-        console.log(currentUser)
+        try {
+            const { data } = await axios.patch(
+                '/api/v1/auth/updateUser',
+                currentUser,
+                { headers: { Authorization: `Bearer ${ state.token }` } }
+            )
+            console.log(data)
+        } catch (error) {
+            console.log(error.response)
+        }
     }
     // manual approach. patch to URL
     // pass in currentUser
