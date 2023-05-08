ed8fecbed0bb28a2f2bd83bec236d73a020eca9f
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-08 13:04:31 +0800

    pseudocode for add job functionality exclude the enum ones first

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -5,5 +5,13 @@ const AddJob = () => {
         </h1>
     )
 }
+// import formRow, alert, useAppContext, wrapper
+// pass in showAlert, displayAlert, position,etc etc
+// do a handleSubmit
+// handle job input
+// then in the return, do the form rows
+// use isEditing to toggle between edit and add job ya
+// submit button... maybe a clear button too
+
 
 export default AddJob
