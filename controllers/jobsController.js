bb4c88d528acf21381443c9cbb2fbf189e2c1cb4
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-09 16:14:10 +0800

    feat: permissions added to disallow a user from editing jobs from other users

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,7 @@
 import Job from '../models/Job.js'
 import { StatusCodes } from 'http-status-codes'
 import { BadRequestError, NotFoundError, UnAuthenticatedError } from '../errors/index.js'
+import checkPermissions from '../utils/checkPermissions.js'
 
 const createJob =  async (req, res) => {
     const { position, company } = req.body
@@ -22,17 +23,17 @@ const getAllJobs =  async (req, res) => {
 const updateJob =  async (req, res) => {
     const { id: jobId } = req.params
     const { company, position } = req.body
-
     if ( !position || !company ) {
         throw new BadRequestError('Please provide all values')
     }
 
     const job = await Job.findOne({ _id: jobId })
-
     if (!job) {
         throw new NotFoundError(`No job with id :${jobId}`)
     }
 
+    checkPermissions(req.user, job.createdBy)
+    
     const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body,
         {
             new: true,
