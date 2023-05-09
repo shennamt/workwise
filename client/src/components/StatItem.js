1324db053248a849762b6242522ca06fa62f55a8
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-10 02:27:14 +0800

    stats components renders, props pass success

--- client/src/components/StatItem.js
+++ client/src/components/StatItem.js
@@ -1,8 +1,14 @@
-const StatItem = () => {
+import Wrapper from '../assets/wrappers/StatItem'
+
+const StatItem = ({ count, title, icon, color, bcg }) => {
 	return (
-		<div>
-			<h2>stat item</h2>
-		</div>
+		<Wrapper color={color} bcg={bcg}>
+			<header>
+				<span className='count'>{count}</span>
+				<span className='icon'>{icon}</span>
+			</header>
+			<h5 className='title'>{title}</h5>
+		</Wrapper>
 	)
 }
 
