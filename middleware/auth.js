3b95b9e16ac197cd75549717065a27c84d5336ea
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-09 11:56:05 +0800

    chore: 401 err on postman

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,16 +3,15 @@ import { UnAuthenticatedError } from '../errors/index.js'
 
 const auth = async (req, res, next) => {
     const authHeader = req.headers.authorization
-    // if doesnt start with bearer, then err
+    console.log(authHeader)
     if (!authHeader || !authHeader.startsWith('Bearer')) {
         throw new UnAuthenticatedError('Oops... Authentication Invalid')
     }
-    // split the string and select the second value AKA token
     const token = authHeader.split(' ')[1]
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET)
         req.user = { userId: payload.userId }
-        next() // if token exp or data is wrong then say 401
+        next()
     } catch {
         throw new UnAuthenticatedError('Oops... Authentication Invalid')
     }
