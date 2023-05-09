901fe493d41fbcb157fb669f022566861dc9f5a9
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-09 21:37:02 +0800

    chore: amended some wrappers for styling issues

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,32 +4,43 @@ import { Logo } from '../components'
 import { Link } from 'react-router-dom'
 
 const Landing = () => {
-    return (
-        <Wrapper>
-            <nav>
-                <Logo />
-            </nav>
-            <div className='container page'>
-                {/* information */}
-                <div className='info'>
-                    <h1><span>WorkWise</span></h1>
-                    <h4>The preferred job tracking app</h4>
-                    <p>
-                    Designed to help job seekers easily track their job applications.
-                    Keep a log of your applications, set reminders for follow-ups, and identify potential areas for improvement.<br/>
-                    <br/>
-                    By keeping all your job search information in one place, WorkWise empowers you to stay organised and focused on landing your dream job.
-                    </p>
-                    <Link to='/register' className='btn btn-hero'>
-                        Begin your journey
-                    </Link>
-                </div>
+	return (
+		<Wrapper>
+			<nav>
+				<Logo />
+			</nav>
+			<div className='container page'>
+				{/* information */}
+				<div className='info'>
+					<h1>
+						<span>WorkWise</span>
+					</h1>
+					<h4>The preferred job tracking app</h4>
+					<p>
+						Designed to help job seekers easily track their job
+						applications. Keep a log of your applications, set
+						reminders for follow-ups, and identify potential areas
+						for improvement.
+						<br />
+						<br />
+						By keeping all your job search information in one place,
+						WorkWise empowers you to stay organised and focused on
+						landing your dream job.
+					</p>
+					<Link to='/register' className='btn btn-hero'>
+						Begin your journey
+					</Link>
+				</div>
 
-                <img src={main} alt='application logging' className='img main-img'/>
-                {/* PS: img is main class and the other class is for styling on smaller media */}
-            </div>
-        </Wrapper>
-    )
+				<img
+					src={main}
+					alt='application logging'
+					className='img main-img'
+				/>
+				{/* PS: img is main class and the other class is for styling on smaller media */}
+			</div>
+		</Wrapper>
+	)
 }
 
 // Unique class from styled components should help reduce possible clashing.
