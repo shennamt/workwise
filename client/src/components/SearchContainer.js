b01e834f07f97e6e24d8a7a37da3539d340a3597
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-10 17:26:53 +0800

    chore: setup + state shows change in context.provider

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,7 +1,44 @@
+import { FormRow, FormRowSelect } from '.'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/SearchContainer'
+
 const SearchContainer = () => {
-    return (
-        <h3>SEARCH COMPONENT</h3>
-    )
+	const {
+		isLoading,
+		search,
+		searchStatus,
+		statusOptions,
+		searchType,
+		jobTypeOptions,
+		searchStyle,
+		jobStyleOptions,
+		sort,
+		sortOptions,
+		handleChange,
+		clearFilters,
+	} = useAppContext()
+
+	// if user sending a req, user wont be able to type something new. so use isLoading
+	const handleSearch = (e) => {
+		if (isLoading) return
+		handleChange({ name: e.target.name, value: e.target.value })
+	}
+
+	return (
+		<Wrapper>
+			<form className='form'>
+				<h4>Search</h4>
+				<div className='form-center'>
+					<FormRow
+						type='text'
+						name='search'
+						value={search}
+						handleChange={handleSearch}
+					/>
+				</div>
+			</form>
+		</Wrapper>
+	)
 }
 
 export default SearchContainer
