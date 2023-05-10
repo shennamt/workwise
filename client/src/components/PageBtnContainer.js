727633aa2fa912964a86b04ab4c5abab93bca7c1
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-10 22:39:34 +0800

    chore: rendering buttons from page numbers

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -4,10 +4,14 @@ import Wrapper from '../assets/wrappers/PageBtnContainer'
 
 const PageBtnContainer = () => {
 	const { numOfPages, page } = useAppContext()
+	const pages = Array.from({ length: numOfPages }, (_, index) => {
+		return index + 1
+	})
 
 	const nextPage = () => {
 		console.log('next page')
 	}
+
 	const prevPage = () => {
 		console.log('prev page')
 	}
@@ -18,7 +22,24 @@ const PageBtnContainer = () => {
 				<BsChevronLeft />
 				Prev
 			</button>
-			<div className='btn-container'>buttons here</div>
+			<div className='btn-container'>
+				{pages.map((pageNumber) => {
+					return (
+						<button
+							type='button'
+							className={
+								pageNumber === page
+									? 'pageBtn active'
+									: 'pageBtn'
+							}
+							key={pageNumber}
+							onClick={() => console.log('change page')}
+						>
+							{pageNumber}
+						</button>
+					)
+				})}
+			</div>
 			<button className='next-btn' onClick={nextPage}>
 				Next
 				<BsChevronRight />
