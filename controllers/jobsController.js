19af99e757ed1ca4c78c7b5d38194a49034e2352
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-10 18:29:34 +0800

    chore: removed hardcode for total jobs and num of pages

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -70,10 +70,13 @@ const getAllJobs = async (req, res) => {
 
 	const jobs = await result
 
+	const totalJobs = await Job.countDocuments(queryObject)
+	const numOfPages = Math.ceil(totalJobs / limit)
+
 	res.status(StatusCodes.OK).json({
 		jobs,
-		totalJobs: jobs.length,
-		numOfPages: 1,
+		totalJobs,
+		numOfPages,
 	})
 }
 
