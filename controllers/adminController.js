f00a65f3c154b5596b90117969025283b2938e93
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-11 19:15:44 +0800

    feat: admin added to req headers finally. works on postman

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,25 +4,24 @@ import User from '../models/User.js'
 import adminPermissions from '../utils/adminPermissions.js'
 
 const getAllUsers = async (req, res) => {
+	console.log('Logged-in user isAdmin:', req.user.isAdmin)
 	const users = await User.find({}).select('-password')
 	res.status(StatusCodes.OK).json({ users })
 }
 
 const deleteUser = async (req, res) => {
 	const { id: userId } = req.params
-	const user = await User.findOne({ _id: userId })
+	const otherUser = await User.findOne({ _id: userId })
 
-	if (!user) {
+	if (!otherUser) {
 		throw new NotFoundError(`No user with id: ${userId}`)
 	}
 
-	if (user.isAdmin) {
+	if (otherUser.isAdmin) {
 		throw new BadRequestError('Admin accounts cannot be deleted')
 	}
 
-	adminPermissions(req.user)
-
-	await user.deleteOne()
+	await otherUser.deleteOne()
 	res.status(StatusCodes.OK).json({ msg: 'Success! User removed...' })
 }
 
--- utils/adminPermissions.js
+++ utils/adminPermissions.js
@@ -1,10 +1,29 @@
+// import { UnAuthenticatedError } from '../errors/index.js'
+
+// const adminPermissions = (requestUser) => {
+// 	if (requestUser.isAdmin) return
+// 	throw new UnAuthenticatedError(
+// 		'You are not authorised to perform this action'
+// 	)
+// }
+
+// export default adminPermissions
+
 import { UnAuthenticatedError } from '../errors/index.js'
 
 const adminPermissions = (requestUser) => {
-	if (requestUser.isAdmin) return
-	throw new UnAuthenticatedError(
-		'You are not authorised to access this route'
-	)
+	console.log(requestUser)
+	console.log('isAdmin', requestUser.isAdmin)
+	// Check if the requestUser exists and has an isAdmin property
+	if (requestUser && requestUser.isAdmin) {
+		// If the user is an admin, return true
+		return true
+	} else {
+		// If the user is not an admin, throw an error
+		throw new UnAuthenticatedError(
+			'You are not authorised to perform this action'
+		)
+	}
 }
 
 export default adminPermissions
