bcbac1bc6b204b9ad4c55b874c3bfc1195ee1cc5
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-11 16:22:57 +0800

    chore: added permissions in admin controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes'
 import { BadRequestError, NotFoundError } from '../errors/index.js'
 import User from '../models/User.js'
-import checkPermissions from '../utils/checkPermissions.js'
+import adminPermissions from '../utils/adminPermissions.js'
 
 const getAllUsers = async (req, res) => {
 	const users = await User.find({}).select('-password')
@@ -20,7 +20,7 @@ const deleteUser = async (req, res) => {
 		throw new BadRequestError('Admin accounts cannot be deleted')
 	}
 
-	checkPermissions(req.user, user._id)
+	adminPermissions(req.user)
 
 	await user.deleteOne()
 	res.status(StatusCodes.OK).json({ msg: 'Success! User removed...' })
