11daa4f08ca8e95de09d7c1ff0ac05d11aa10afd
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-11 14:51:59 +0800

    chore: changed the auth js. was wrong prev

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ const auth = async (req, res, next) => {
 		const payload = jwt.verify(token, process.env.JWT_SECRET)
 		req.user = {
 			userId: payload.userId,
-			isAdmin: true,
+			isAdmin: payload.isAdmin,
 		}
 		next()
 	} catch {
