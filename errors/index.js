3137efa4669996ba6aca0db1c093a73f44293f56
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-11 16:43:05 +0800

    chore: removed old err from index

--- errors/index.js
+++ errors/index.js
@@ -1,10 +1,4 @@
 import BadRequestError from './bad-request.js'
 import NotFoundError from './not-found.js'
 import UnAuthenticatedError from './unauthenticated.js'
-import UnAuthorizedError from './unauthorized.js'
-export {
-	BadRequestError,
-	NotFoundError,
-	UnAuthenticatedError,
-	UnAuthorizedError,
-}
+export { BadRequestError, NotFoundError, UnAuthenticatedError }
