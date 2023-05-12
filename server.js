5694c5defcb002c3e520b8562d9bf55f55953ee2
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-13 05:20:49 +0800

    err: doesnt show on server

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ import morgan from 'morgan'
 const app = express()
 dotenv.config()
 
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
 // db and auth
 import connectDB from './db/connect.js'
 
@@ -25,19 +29,17 @@ if (process.env.NODE_ENV !== 'production') {
 	app.use(morgan('dev'))
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.resolve(__dirname, '.client/build')))
 app.use(express.json())
-
-app.get('/', (req, res) => {
-	res.json({ msg: 'The healthy server life chose YOU!' })
-})
-
-app.get('/api/v1', (req, res) => {
-	res.json({ msg: 'API' })
-})
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 app.use('/api/v1/admin', authenticateUser, adminRouter)
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
