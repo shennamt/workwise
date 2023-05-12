e39eb762f36cbdb0634be07093c39b62edd88ccd
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-12 12:44:52 +0800

    chore: conditionally rendering side bar

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,22 +1,24 @@
 import { Outlet } from 'react-router-dom'
 import Wrapper from '../../assets/wrappers/SharedLayout'
 import { Navbar, BigSidebar, SmallSidebar } from '../../components'
+import { useAppContext } from '../../context/appContext'
 
 const SharedLayout = () => {
-    return (
-        <Wrapper>
-            <main className='dashboard'>
-                <SmallSidebar />
-                <BigSidebar />
-                <div>
-                    <Navbar />
-                    <div className='dashboard-page'>
-                        <Outlet />
-                    </div>
-                </div>
-            </main>
-        </Wrapper>
-    )
+	const { user } = useAppContext()
+	return (
+		<Wrapper>
+			<main className='dashboard'>
+				{user && !user.isAdmin && <SmallSidebar />}
+				{user && !user.isAdmin && <BigSidebar />}
+				<div>
+					<Navbar />
+					<div className='dashboard-page'>
+						<Outlet />
+					</div>
+				</div>
+			</main>
+		</Wrapper>
+	)
 }
 
 export default SharedLayout
