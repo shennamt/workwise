d8f761fbb827093f489c23611a605d7c20a67adb
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-12 15:25:03 +0800

    choreL added admin dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import {
 	SharedLayout,
 	Stats,
 	AllUsers,
+	AdminDash,
 } from './pages/dashboard'
 import { useAppContext } from './context/appContext'
 
@@ -26,7 +27,13 @@ function App() {
 					{user && (
 						<>
 							{user.isAdmin ? (
-								<Route index element={<AllUsers />} />
+								<>
+									<Route index element={<AdminDash />} />
+									<Route
+										path='admin'
+										element={<AllUsers />}
+									/>
+								</>
 							) : (
 								<>
 									<Route index element={<Stats />} />
--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -4,5 +4,6 @@ import Profile from './Profile'
 import SharedLayout from './SharedLayout'
 import Stats from './Stats'
 import AllUsers from './AllUsers'
+import AdminDash from './AdminDash'
 
-export { AddJob, AllJobs, Profile, SharedLayout, Stats, AllUsers }
+export { AddJob, AllJobs, Profile, SharedLayout, Stats, AllUsers, AdminDash }
