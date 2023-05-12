f06fc48a080ebd132ce22c79634f5bfe0379c66d
Shenna Mae Tan <shenmae.t@gmail.com>
2023-05-12 12:54:24 +0800

    chore: conditionally render the nav icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,46 +6,52 @@ import { useState } from 'react'
 import Logo from './Logo'
 
 const Navbar = () => {
-    const [showLogout, setShowLogout] = useState(false)
-    const { toggleSidebar, logoutUser, user } = useAppContext()
+	const [showLogout, setShowLogout] = useState(false)
+	const { toggleSidebar, logoutUser, user } = useAppContext()
 
-    return (
-        <Wrapper>
-            <div className='nav-center'>
-                <button
-                    type='button'
-                    className='toggle-btn'
-                    onClick={ toggleSidebar }
-                >
-                    <FaAlignLeft />
-                </button>
-                <div>
-                    <Logo />
-                    <h3 className='logo-text'>Dashboard</h3>
-                </div>
-                <div className='btn-container'>
-                    <button
-                        type='button'
-                        className='btn'
-                        onClick={() => setShowLogout(!showLogout)}
-                    >
-                        <FaUserCircle />
-                        {user?.name}
-                        <FaCaretDown />
-                    </button>
-                    <div className={ showLogout ? 'dropdown show-dropdown' : 'dropdown' }>
-                        <button
-                            type='button'
-                            className='dropdown-btn'
-                            onClick={logoutUser}
-                        >
-                            logout
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </Wrapper>
-    )
+	return (
+		<Wrapper>
+			<div className='nav-center'>
+				{!user?.isAdmin && (
+					<button
+						type='button'
+						className='toggle-btn'
+						onClick={toggleSidebar}
+					>
+						<FaAlignLeft />
+					</button>
+				)}
+				<div>
+					<Logo />
+					<h3 className='logo-text'>Dashboard</h3>
+				</div>
+				<div className='btn-container'>
+					<button
+						type='button'
+						className='btn'
+						onClick={() => setShowLogout(!showLogout)}
+					>
+						<FaUserCircle />
+						{user?.name}
+						<FaCaretDown />
+					</button>
+					<div
+						className={
+							showLogout ? 'dropdown show-dropdown' : 'dropdown'
+						}
+					>
+						<button
+							type='button'
+							className='dropdown-btn'
+							onClick={logoutUser}
+						>
+							logout
+						</button>
+					</div>
+				</div>
+			</div>
+		</Wrapper>
+	)
 }
 
 export default Navbar
